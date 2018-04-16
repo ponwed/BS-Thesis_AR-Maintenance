c8c50255d215cb7412599585226724043857c90c
Pontus <pontus.wed@gmail.com>
2018-04-16 14:49:13 +0200

    Added markers, setup rendering control script for camera

---
+++ AR-maintenance-app/Assets/RenderControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class RenderControl : MonoBehaviour {
+
+    private StateManager sm;
+    private IEnumerable<TrackableBehaviour> trackables;
+    private TrackableBehaviour trackableBehaviour;
+
+	void Start () {
+
+        trackableBehaviour = GetComponent<TrackableBehaviour>();
+        GameObject[] markers = GameObject.FindGameObjectsWithTag("Marker");
+
+        foreach (var marker in markers)
+        {
+            for (int i = 0; i < marker.transform.childCount; i++)
+            {
+                marker.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+	
+	void Update () {
+        sm = TrackerManager.Instance.GetStateManager();
+        trackables = sm.GetActiveTrackableBehaviours();
+
+        foreach (var tb in trackables)
+        {
+            if (tb.name == "MarkerLeft")
+            {
+                for (int i = 0; i < tb.transform.childCount; i++)
+                {
+                    tb.transform.GetChild(i).gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+}
