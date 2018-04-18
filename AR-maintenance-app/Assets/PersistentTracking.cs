987cdb5549a2494e60734285e7089eb85e29b6ba
Pontus <pontus.wed@gmail.com>
2018-04-18 13:10:04 +0200

    New world center + option for persistent tracking

---
+++ AR-maintenance-app/Assets/PersistentTracking.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class PersistentTracking : MonoBehaviour {
+
+	void Start () {
+        VuforiaARController.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+	}
+
+	void OnVuforiaStarted () {
+        ObjectTracker tracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+        bool success = tracker.PersistExtendedTracking(true);
+
+        Debug.Log("PTracking: " + success);
+	}
+}
