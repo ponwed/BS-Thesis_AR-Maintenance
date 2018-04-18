8ff2650297f50da8bcb5bfec0b1a56d6e1aa1d11
deerel <deerel@gmail.com>
2018-04-18 08:58:14 +0200

    ViewState and button

---
+++ AR-maintenance-app/Assets/Scripts/ScriptBtnNext.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptBtnNext : MonoBehaviour {
+
+    private Button myselfButton;
+    public ViewState state;
+    public bool isNext;
+
+    // Use this for initialization
+    void Start () {
+        var btn = GetComponent<RectTransform>();
+        int size = Screen.width / 2;
+        btn.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size);
+        btn.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, size/4);
+
+        myselfButton = GetComponent<Button>();
+        myselfButton.onClick.AddListener(() => ActionClick());
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ActionClick()
+    {
+        if (isNext)
+            state.Next();
+        else
+            state.Previous();
+    }
+}
---
+++ AR-maintenance-app/Assets/Scripts/ViewState.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewState : MonoBehaviour {
+    
+    int state;
+    int stateCount;
+    Transform marker;
+    public GameObject tracker; // Passed via Unity GUI
+
+    /// <summary>
+    /// Initialization of the instance.
+    /// </summary>
+    void Start () {
+        state = 0;
+        try
+        {
+            marker = tracker.transform;
+        } catch(Exception e)
+        {
+            Debug.Log("No Tracker defined.");
+            throw e;
+        }
+        stateCount = marker.childCount;
+        Toggle();
+        Debug.Log("View state starting ");
+    }
+	
+	/// <summary>
+    /// Called every frame.
+    /// </summary>
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Go to next state.
+    /// </summary>
+    public void Next()
+    {
+        if (state >= stateCount - 1) return;
+        state++;
+        Toggle();
+        Debug.Log("Next state: " + state);
+    }
+
+    /// <summary>
+    /// Go to previous state.
+    /// </summary>
+    public void Previous()
+    {
+        if (state <= 0) return;
+        state--;
+        Toggle();
+        Debug.Log("Previous state " + state);
+    }
+
+    /// <summary>
+    /// Changes what to render based on the state.
+    /// </summary>
+    void Toggle()
+    {
+        for(var i = 0; i < stateCount; i++)
+        {
+            bool visible = false;
+            var child = marker.GetChild(i);
+
+            if (i == state) visible = true;
+
+            foreach(var r in child.GetComponentsInChildren<Renderer>(true))
+            {
+                r.enabled = visible;
+            }
+        }
+    }
+
+}
