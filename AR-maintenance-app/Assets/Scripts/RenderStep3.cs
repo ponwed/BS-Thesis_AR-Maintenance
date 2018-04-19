8756505ef4a83955889a295a9fd276e7905b0a6f
Pontus <pontus.wed@gmail.com>
2018-04-19 11:24:15 +0200

    Step 3 v1

---
+++ AR-maintenance-app/Assets/Scripts/RenderStep3.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using System.Linq;
+
+public class RenderStep3 : MonoBehaviour {
+
+    public ViewState viewState;
+    public GameObject tracker;
+    public GameObject indicatorCircle;
+    private Transform marker;
+    private bool visible;
+
+    void Start()
+    {
+        marker = tracker.transform;
+        ToggleSprites(false);
+    }
+
+    void Update()
+    {
+        /* If step should be visible, start looking if sleigh marker is active */
+        if (visible)
+        {
+            StateManager sm;
+            IEnumerable<TrackableBehaviour> trackables;
+
+            sm = TrackerManager.Instance.GetStateManager();
+            trackables = sm.GetActiveTrackableBehaviours();
+
+            if (trackables.FirstOrDefault(GameObject => GameObject.name == tracker.name))
+            {
+                //indicatorCircle.GetComponent<Renderer>().enabled = false;
+                indicatorCircle.transform.GetComponent<Renderer>().enabled = false;
+
+                for (int i = 0; i < marker.childCount; i++)
+                {
+                    ToggleSprites(visible);
+                }
+            }
+            else
+                indicatorCircle.transform.GetComponent<Renderer>().enabled = true;
+        }
+        else
+        {
+            ToggleSprites(false);
+        }
+    }
+
+    private void ToggleSprites(bool b)
+    {
+        for (int i = 0; i < marker.childCount; i++)
+        {
+            if (marker.GetChild(i).childCount == 0)
+                marker.GetChild(i).GetComponent<Renderer>().enabled = b;
+            else
+                marker.GetChild(i).GetChild(0).GetComponent<SpriteRenderer>().enabled = b;
+        }
+    }
+
+    public void Render(bool render)
+    {
+        visible = render;
+    }
+}
