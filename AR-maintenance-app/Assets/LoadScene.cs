4e149ecf45e6d43b91f8b21c85f7f75e34d07306
Pontus <pontus.wed@gmail.com>
2018-04-24 13:22:19 +0200

    Added tutorial scene

---
+++ AR-maintenance-app/Assets/LoadScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(LoadARScene);
+	}
+	
+	void LoadARScene()
+    {
+        SceneManager.LoadScene("AR-main");
+    }
+}
