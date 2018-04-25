cd3386b1b0fc551b540064f9244fb1a97374309e
Pontus <pontus.wed@gmail.com>
2018-04-25 14:12:45 +0200

    Working on compass

---
+++ AR-maintenance-app/Assets/DirectionGuide.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionGuide : MonoBehaviour {
+
+    public Transform target;
+    private Vector3 pos;
+	// Use this for initialization
+	void Start () {
+        pos = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 up = new Vector3(0, 1, 0);
+
+        transform.LookAt(target, up);
+
+        //Vector3 cameraPos = Camera.main.transform.position;
+        //Vector3 dirVector = pos - cameraPos;
+
+        //Debug.Log(dirVector);
+	}
+}
