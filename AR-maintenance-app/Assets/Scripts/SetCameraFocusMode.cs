a1d960a11ed6aab12a64ddea0569194c0d99d63a
Pontus <pontus.wed@gmail.com>
2018-04-27 10:03:49 +0200

    Camera focus mode script updated + finished step added

--- AR-maintenance-app/Assets/Scripts/SetCameraFocusMode.cs
+++ AR-maintenance-app/Assets/Scripts/SetCameraFocusMode.cs
@@ -3,24 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
+public enum FocusModes
+{
+    FOCUS_MODE_NORMAL,
+    FOCUS_MODE_TRIGGERAUTO,
+    FOCUS_MODE_CONTINUOUSAUTO,
+    FOCUS_MODE_INFINITY,
+    FOCUS_MODE_MACRO
+}
+
 public class SetCameraFocusMode : MonoBehaviour {
+
+    public FocusModes focusMode;
+    private bool mVuforiaStarted = false;
 
 	void Start () {
         var vuforia = VuforiaARController.Instance;
         vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
-        vuforia.RegisterOnPauseCallback(OnPaused);
+        vuforia.RegisterOnPauseCallback(OnApplicationPause);
     }
 
     private void OnVuforiaStarted()
     {
-        CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        mVuforiaStarted = true;
+        SetFocusMode();
     }
 
-    private void OnPaused(bool paused)
+    private void OnApplicationPause(bool pause)
     {
-        if (!paused)
+        if (!pause)
         {
-            CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+            if (mVuforiaStarted)
+            {
+                SetFocusMode();
+            }
+        }
+    }
+
+    private void SetFocusMode()
+    {
+        switch (focusMode)
+        {
+            case FocusModes.FOCUS_MODE_NORMAL:
+                CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_NORMAL);
+                break;
+            case FocusModes.FOCUS_MODE_TRIGGERAUTO:
+                CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_TRIGGERAUTO);
+                break;
+            case FocusModes.FOCUS_MODE_CONTINUOUSAUTO:
+                CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+                break;
+            case FocusModes.FOCUS_MODE_INFINITY:
+                CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_INFINITY);
+                break;
+            case FocusModes.FOCUS_MODE_MACRO:
+                CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_MACRO);
+                break;
+            default:
+                CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_NORMAL);
+                break;
         }
     }
 }
